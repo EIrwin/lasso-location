7267b2b6717590df57f56ecc9b9d49ea18af15ec
Joe Kelly <joe.kelly@outlook.com>
2015-03-07 22:35:17 -0700

    added initial project logic

--- app.js
+++ app.js
@@ -1,60 +1,38 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var socket = require('express.io')();
+var moment = require('moment');
+var settings = require('./settings');
+var schedule = require('node-schedule');
+var geolib = require('geolib');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+function locate(room){
+    room.broadcast('locate');
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+socket.http().io();
+
+socket.io.route('init', function(req){
+    req.io.join(req.data);
+    req.io.join(req.data + ':ctrl');
+    var room = req.io.room(req.data + ':ctrl');
+    locate(room);
 });
 
+socket.io.route('announce', function(req){
 
-module.exports = app;
+    //fetch nearest coordinate to self given an array of lassos and friends coordinates.
+
+    //determine distance between self and nearest coordinate in feet.
+    var distance = 5200;
+
+    //seconds until next update = (distance / resolution)^throttle where throttle is less than 1.
+    var seconds = Math.round(Math.pow(distance / settings.resolution, settings.throttle));
+
+    //schedule locate broadcast for the control room.
+    var room = req.io.room(req.data.id + ':ctrl');
+    var job = new schedule.Job(function(){ locate(room); });
+    job.schedule(moment().add(seconds, 'seconds'));
+
+    req.io.room(req.data.id).broadcast('update', req.data);
+});
+
+module.exports = socket;
